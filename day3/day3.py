309aa07419576efcec806e6c49faf09a503e2a7a
Chase Kelley <makinbakonpancakes@gmail.com>
2016-12-03 14:45:06 -0500

    refactoring to print both solutions

--- day3/day3.py
+++ day3/day3.py
@@ -7,14 +7,16 @@ triangles_2 = []
 for col in columns:
     triangles_2.extend([[col[x], col[x+1], col[x+2]]
                         for x in range(0, len(col)-1, 3)])
-not_triangles = []
-yes_triangles = []
-# to switch parts, change to triangles_1
-for tri in triangles_2:
-    a, b, c = tri[0], tri[1], tri[2]
-    if a+b > c and a+c > b and b+c > a:
-        yes_triangles.append(tri)
-    else:
-        not_triangles.append(tri)
-print(len(yes_triangles))
-print(len(not_triangles))
+
+
+def triangle_palooza(triangles):
+    yes_triangles = []
+    for tri in triangles:
+        a, b, c = tri[0], tri[1], tri[2]
+        if a+b > c and a+c > b and b+c > a:
+            yes_triangles.append(tri)
+    return len(yes_triangles)
+
+
+print(triangle_palooza(triangles_1))
+print(triangle_palooza(triangles_2))
